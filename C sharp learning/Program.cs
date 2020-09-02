cc9f26d7871a7a62b7ac4d8da47e5db99180b545
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-02 17:21:39 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C_sharp_learning
+{
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+            Console.WriteLine("__________________________" + "  for");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + "\n");
+            }
+            Console.WriteLine("__________________________" + "  continue");
+            for (int i = 10; i >=5; i--)
+            {
+                if (i == 8)
+                    continue; //пропустил итерацию
+                    //break выйдет из цикла for
+                Console.WriteLine(i + "\n");
+            }
+
+            Console.WriteLine("__________________________" + "  While");
+            int key = 0;
+
+            while(key <5)
+            {
+                Console.WriteLine(key);
+                key++;
+            }
+            Console.WriteLine("__________________________" + "  Do While");
+            int Liora;
+            do
+            {
+                Console.WriteLine("Enter Number");
+                Liora = Convert.ToInt32(Console.ReadLine());
+            } while (Liora != 5);
+            {
+                Console.WriteLine("Good. You write 5.");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
